0e9c68082940b63061aedf79502d5737e2f40a77
Corentin <corentin.damas@gmail.com>
2024-07-04 15:18:18 +0200

    Api server init

---
+++ app.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ app: "Natours", mesage: "Hello from the server side!" });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "sussess",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length-1].id + 1
+  const newTour = Object.assign({id:newId}, req.body)
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
+    res.status(201).json({
+        status:"success",
+        data:{
+            tour:newTour
+        }
+    })
+  })
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});

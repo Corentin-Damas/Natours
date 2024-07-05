30a30168ca7cf2b2ecbd421c6c1ebe0577eaaf3e
Corentin <corentin.damas@gmail.com>
2024-07-05 12:39:43 +0200

    app architecture set up

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ See result:  https://corentin-damas.github.io/Natours/
 ### Previews:
 ![image](https://github.com/Corentin-Damas/Natours/assets/100703359/bedb8ed9-915f-4cc4-a674-167ef5a8511b)
 ![image](https://github.com/Corentin-Damas/Natours/assets/100703359/e9932127-c825-490d-9fc0-dd9e65f07e1b)
+
+
+// Create a new MangDB database
+
+const database = 'Natours';
+const collection = 'tours';
+use(database);
+db.createCollection(collection);
--- app.js
+++ app.js
@@ -1,45 +1,20 @@
-const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
+//middlewear
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     .json({ app: "Natours", mesage: "Hello from the server side!" });
-// });
+app.use(express.static(`${__dirname}/public`));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-app.get("/api/v1/tours", (req, res) => {
-  res.status(200).json({
-    status: "sussess",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length-1].id + 1
-  const newTour = Object.assign({id:newId}, req.body)
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
-    res.status(201).json({
-        status:"success",
-        data:{
-            tour:newTour
-        }
-    })
-  })
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;

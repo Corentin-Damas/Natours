e00be9c795a303713dbe75e78381de3eb91b04e5
Corentin <corentin.damas@gmail.com>
2024-07-08 17:44:18 +0200

    error handlers and user setup

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,87 +1,155 @@
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const tours = await Tour.find();
-    res.status(200).json({
-      status: "sussess",
-      result: tours.length,
-      data: {
-        tours,
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingAverage,summary,difficulty";
+  next();
+};
+
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .pagination();
+  const tours = await features.query;
+
+  res.status(200).json({
+    status: "sussess",
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "sussess",
+    data: {
+      tour,
+    },
+  });
+});
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: newTour,
+    },
+  });
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError("No tour found with that ID", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: "$difficulty" },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: "$ratingsQuantity" },
+        avgRating: { $avg: "$ratingsAverage" },
+        avgPrice: { $avg: "$price" },
+        minPrice: { $min: "$price" },
+        maxPrice: { $max: "$price" },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id);
-    res.status(200).json({
-      status: "sussess",
-      data: {
-        tour,
+    },
+    {
+      $sort: { avgPrice: 1 },
+    },
+  ]);
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats,
+    },
+  });
+});
+
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
+  const plan = await Tour.aggregate([
+    {
+      $unwind: "$startDates",
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: newTour,
+    },
+    {
+      $group: {
+        _id: { $month: "$startDates" },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: "$name" },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid data sent!",
-    });
-  }
-};
-
-exports.updateTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        tour,
+    },
+    {
+      $addFields: { month: "$_id" },
+    },
+    {
+      $project: {
+        _id: 0,
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid data sent!",
-    });
-  }
-};
+    },
+    {
+      $sort: { numTourStarts: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid data we couldn't delete !",
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      plan,
+    },
+  });
+});
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,102 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const validator = require("validator");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "A tour must have a name"],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    require: [true, "A tour must have a duration"],
-  },
-  maxGroupeSize: {
-    type: Number,
-    require: [true, "A tour must have a group size"],
-  },
-  difficulty: {
-    type: String,
-    require: [true, "A tour must have a diffiulty"],
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+      maxLenght: [40, "A tour name must have less or equal then 40 characters"],
+      minLenght: [10, "A tour name must have more or equal then 10 characters"],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      require: [true, "A tour must have a duration"],
+    },
+    maxGroupeSize: {
+      type: Number,
+      require: [true, "A tour must have a group size"],
+    },
+    difficulty: {
+      type: String,
+      require: [true, "A tour must have a diffiulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty should be easer : easy, medium, difficult",
+      },
+    },
 
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be bellow 5.0"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: [true, "A tour must have a price"],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) should be below regular price",
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      require: [true, "A tour must have a summary"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    require: [true, "A tour must have a price"],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    require: [true, "A tour must have a summary"],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have a cover image"],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+// Add a field on Get not in the db
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
 });
+
+// Doc middleware: run before save and create
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Query MiddleWare
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
